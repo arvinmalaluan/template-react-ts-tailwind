514d11ccd63ddc1fd3124ff2b21202934ec67977
Arvin Malaluan <arvinmalaluan7@gmail.com>
2024-06-28 22:03:07 +0800

    uploading-of-boiler-template

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="h-screen w-screen flex flex-col items-center justify-center">
+      <div className="text-center flex flex-col items-center">
+        <img
+          src="https://i.pinimg.com/originals/c0/30/03/c03003776e9cb3e20ba7bd3171700507.gif"
+          alt=""
+        />
+        <p className="text-3xl font-medium">Greetings!</p>
+        <p className="mt-2">
+          This is a template that uses react, tailwind, and react router dom.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
